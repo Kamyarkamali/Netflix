6f3cd2d6fa77d94dccbd9887b16d1ec2c6a70803
kamyarkamali <kamyarkamali021@gmail.com>
2024-05-23 19:34:37 +0330

    debug

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
-import {FaHeart,FaRegHeart} from "react-icons/fa";
+import React, { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-
-const Movies = ({item}) => {
-    const [like,setLike]=useState(false);
-    return (
-        <div className='w-[160px] sm:w-[150px] md:w-[240px] lg:w-[280px] inline-block relative cursor-pointer p-2'>
-                            <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-                            <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 duration-300 opacity-0 text-white hover:opacity-100'>
-                                <p className='whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
-                                <p>
-                                    {like ? <FaHeart className='absolute text-gray-400 top-4 left-4'/> : <FaRegHeart className='absolute text-gray-400 top-4 left-4'/>}
-                                </p>
-                            </div>
-                        </div>
-    );
+const Movies = ({ item }) => {
+  const [like, setLike] = useState(false);
+  return (
+    <div className="w-[160px] sm:w-[150px] md:w-[240px] lg:w-[280px] inline-block relative cursor-pointer p-2">
+      <img
+        className="w-full h-auto block"
+        src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+        alt={item?.title}
+      />
+      <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 duration-300 opacity-0 text-white hover:opacity-100">
+        <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+          {item?.title}
+        </p>
+        <p>
+          {like ? (
+            <FaHeart className="absolute text-gray-400 top-4 left-4" />
+          ) : (
+            <FaRegHeart className="absolute text-gray-400 top-4 left-4" />
+          )}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Movies;
--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -1,48 +1,58 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 //Icons
-import { MdChevronLeft,MdChevronRight } from "react-icons/md";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 //component
-import Movies from '../Movies/Movies';
+import Movies from "../Movies/Movies";
 
-const Row = ({title,fetchURL,id}) => {
-    const [movies,setMovies]=useState([]);
+const Row = ({ title, fetchURL, id }) => {
+  const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
 
-    useEffect(()=>{
-        axios.get(fetchURL).then((response)=>{
-            setMovies(response.data.results)
-        })
-    },[fetchURL])
+  //ScrollLeft
+  const scrollLeft = () => {
+    let slider = document.getElementById("slider" + id);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
 
-    //ScrollLeft
-    const scrollLeft=()=>{
-        let slider=document.getElementById("slider"+id)
-        slider.scrollLeft=slider.scrollLeft-500
-    }
+  //ScorollRight
+  const scrollRight = () => {
+    let sliders = document.getElementById("slider" + id);
+    sliders.scrollLeft = sliders.scrollLeft + 500;
+  };
 
-    //ScorollRight
-    const scrollRight=()=>{
-        let sliders=document.getElementById("slider"+id)
-        sliders.scrollLeft=sliders.scrollLeft+500
-    }
-
-    return (
-        <>
-            <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-            <div className='relative flex items-center group'>
-                <MdChevronLeft onClick={scrollLeft} size={30} className='bg-white rounded-full opacity-50 hover:opacity-100 duration-300 cursor-pointer z-10 hidden group-hover:block'/>
-                <div id={"slider"+id} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scroll-hide relative duration-300'>
-                    {movies.map((item,id)=>(
-                        <Movies item={item} key={id}/>
-                    ))}
-                </div>
-                <MdChevronRight onClick={scrollRight} size={30} className='bg-white rounded-full opacity-50 hover:opacity-100 duration-300 cursor-pointer z-10 hidden group-hover:block'/>
-            </div>
-            </>
-    );
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center group duration-300">
+        <MdChevronLeft
+          onClick={scrollLeft}
+          size={30}
+          className="bg-white rounded-full opacity-50 hover:opacity-100 duration-300 cursor-pointer z-10  group-hover:block"
+        />
+        <div
+          id={"slider" + id}
+          className="w-full h-full overflow-x-hidden whitespace-nowrap scroll-smooth scroll-hide relative duration-300"
+        >
+          {movies.map((item, id) => (
+            <Movies item={item} key={id} />
+          ))}
+        </div>
+        <MdChevronRight
+          onClick={scrollRight}
+          size={30}
+          className="bg-white  rounded-full opacity-50 hover:opacity-100 transition-all ease-linear duration-300 cursor-pointer group-hover:block"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Row;
